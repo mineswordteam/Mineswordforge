3f658c9f44e53d8caf1057229c3463fa572244de
mineswordteam <nikannourieh@gmail.com>
2026-09-13 18:34:41 +0330

    Initial Minesword Forge Electron source

---
+++ main.js
@@ -0,0 +1,44 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    minWidth: 960,
+    minHeight: 600,
+    icon: path.join(__dirname, 'build', 'icon.ico'),
+    title: 'MineSword Forge',
+    autoHideMenuBar: true,
+    backgroundColor: '#000000',
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+      sandbox: true,
+    },
+  });
+
+  Menu.setApplicationMenu(null);
+
+  mainWindow.loadFile(path.join(__dirname, 'app', 'index.html'));
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
